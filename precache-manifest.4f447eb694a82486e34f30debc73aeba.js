e2330d62554fc839fa586c2fc4c6b2581bfb991a
Youjung <guest@GUESTs-MacBook-Air.local>
2020-01-09 13:59:41 -0800

    Updates

---
+++ precache-manifest.4f447eb694a82486e34f30debc73aeba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad83959f504bcfb064c16af698617ae8",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "1ef62a8d6078a4b12087",
+    "url": "/movie_app/static/css/main.14ecae94.chunk.css"
+  },
+  {
+    "revision": "ab7b7ef23752be3c322d",
+    "url": "/movie_app/static/js/2.6a7380e5.chunk.js"
+  },
+  {
+    "revision": "1ef62a8d6078a4b12087",
+    "url": "/movie_app/static/js/main.8c8eee4b.chunk.js"
+  },
+  {
+    "revision": "8971f8699b511f3e262b",
+    "url": "/movie_app/static/js/runtime-main.2890315e.js"
+  }
+]);
